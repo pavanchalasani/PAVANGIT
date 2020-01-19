5db97545c407a882927a15e70e044155309949a5
pavanchalasani <58877577+pavanchalasani@users.noreply.github.com>
2020-01-19 17:56:00 +0530

    Updated display comment

--- src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
+++ src/main/java/works/buddy/samples/WorksWithHerokuServlet.java
@@ -13,7 +13,7 @@ public class WorksWithHerokuServlet extends HttpServlet {
         response.setContentType("text/plain");
         response.setStatus(404);
         PrintWriter writer = response.getWriter();
-        writer.print("Sample Demo Page as part of Super League DevOps Sessions");
+        writer.print("Sample Demo Page as part of Super League DevOps Sessions covering CI and CD activities");
         writer.close();
     }
 }
