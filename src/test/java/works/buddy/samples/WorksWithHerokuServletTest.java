516315452f2e431ed4fe1d2bea17303aa55275da
pavanchalasani <58877577+pavanchalasani@users.noreply.github.com>
2020-01-25 19:23:33 +0530

    Changes to Sample test

--- src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
+++ src/test/java/works/buddy/samples/WorksWithHerokuServletTest.java
@@ -36,6 +36,6 @@ public class WorksWithHerokuServletTest {
         when(response.getWriter()).thenReturn(writer);
 
         servlet.doGet(request, response);
-        assertEquals("Sample Demo Page as part of Super League DevOps Sessions covering CI and CD activities", new String( out.toByteArray(), "UTF-8"));
+        assertEquals("Sample Demo Page as part of Super League DevOps Sessions covering CI and CD activitiesSample Demo Page as part of Super League DevOps Sessions covering CI and CD activities including Nexus as artifactory tool, SonarQube for code quality", new String( out.toByteArray(), "UTF-8"));
     }
 }
